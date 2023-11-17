384a6f057bcdef39bfbcf9104f5b5680f30b4bb5
Sude Ateşoglu <sude.atesoglu4@gmail.com>
2023-11-17 15:19:38 +0300

    counter class added

--- reentrantlock/Counter.java
+++ reentrantlock/Counter.java
@@ -8,7 +8,7 @@ public class Counter extends Thread {
 	private HashMap<String, Integer> hMap;
 	private ReentrantLock rtLock;
 
-	// constructor to initialize
+	// constructor for initializing
 	public Counter(String[] array, HashMap<String, Integer> hMap, ReentrantLock rtLock) {
 		// TODO Auto-generated constructor stub
 		this.array = array;
@@ -34,26 +34,38 @@ public class Counter extends Thread {
 		HashMap<String, Integer> hashMap = new HashMap<>();
 		ReentrantLock rtLock = new ReentrantLock();
 		
-		// create two arrays that repeates the letter 400 times
+		// create two arrays that repeates the same letter 400 times
 		String[] array1 = new String[400];
 		String[] array2 = new String[400];
 		for (int i = 0; i < 400; i++) {
 			array1[i] = "a";
-	        array2[i] = "a";
+			array2[i] = "a";
 		}
 		
-		// start and join the threads
-		Counter counter = new Counter(array1, hashMap, rtLock);
-		Counter counter2 = new Counter(array2, hashMap, rtLock);
-		
-		counter.start();
-		counter2.start();
-		
-		counter.join();
-		counter2.join();
-		
-		System.out.println(hashMap);  // print the counts
-		// {a=800}
-	}
+		// create another array mix of different letters
+		String[] array3 = {"x", "y", "z", "x", "y", "z", "x", "x", "x"};
 
+		// start and join the threads
+		Counter counter1 = new Counter(array1, hashMap, rtLock);
+		Counter counter2 = new Counter(array2, hashMap, rtLock);
+		Counter counter3 = new Counter(array3, hashMap, rtLock);
+		
+		counter1.start();
+		counter2.start();
+		counter3.start();
+		
+		counter1.join();
+		counter2.join();
+		counter3.join();
+		
+		// print the counts
+		for (String key : hashMap.keySet()) {
+			System.out.println(key + ": " +hashMap.get(key));  
+		}
+		
+		/* a: 800
+		   x: 5
+		   y: 2
+		   z: 2 */
+	}
 }
